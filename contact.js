ca94e2a2b88d66b8869c5f22b51ccf4596f2ea91
omeatai <omeatai@gmail.com>
2021-11-22 18:37:26 +0100

    1 file modified

--- contact.js
+++ contact.js
@@ -1,35 +1,32 @@
 import React from 'react';
-import Contact from './Contact';
 
-function App() {
+export default function Contact(props) {
+  /**
+   * Challenge: Fix the code below to use the `props`
+   * object values in place of the hardcoded values below
+   */
   return (
-    <div className="contacts">
-      <Contact
-        img="./images/mr-whiskerson.png"
-        name="Mr. Whiskerson"
-        phone="(212) 555-1234"
-        email="mr.whiskaz@catnap.meow"
-      />
-      <Contact
-        img="./images/fluffykins.png"
-        name="Fluffykins"
-        phone="(212) 555-2345"
-        email="fluff@me.com"
-      />
-      <Contact
-        img="./images/felix.png"
-        name="Felix"
-        phone="(212) 555-4567"
-        email="thecat@hotmail.com"
-      />
-      <Contact
-        img="./images/pumpkin.png"
-        name="Pumpkin"
-        phone="(0800) CAT KING"
-        email="pumpkin@scrimba.com"
-      />
+    <div className="contact-card">
+      <img src={props.img} />
+      <h3>{props.name}</h3>
+      <div className="info-group">
+        <img src="./images/phone-icon.png" />
+        <p>{props.phone}</p>
+      </div>
+      <div className="info-group">
+        <img src="./images/mail-icon.png" />
+        <p>{props.email}</p>
+      </div>
     </div>
   );
 }
 
-export default App;
+/* 
+{
+    img: "./images/mr-whiskerson.png", 
+    name: "Mr. Whiskerson", 
+    phone: "(212) 555-1234", 
+    email: "mr.whiskaz@catnap.meow"
+}
+
+*/
