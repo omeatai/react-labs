45f50c917f5d87facf23f3c80449731d606c906c
omeatai <omeatai@gmail.com>
2021-11-22 18:36:42 +0100

    1 file modified

--- App.js
+++ App.js
@@ -1,11 +1,35 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import Contact from './Contact';
 
-export default function App() {
+function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div className="contacts">
+      <Contact
+        img="./images/mr-whiskerson.png"
+        name="Mr. Whiskerson"
+        phone="(212) 555-1234"
+        email="mr.whiskaz@catnap.meow"
+      />
+      <Contact
+        img="./images/fluffykins.png"
+        name="Fluffykins"
+        phone="(212) 555-2345"
+        email="fluff@me.com"
+      />
+      <Contact
+        img="./images/felix.png"
+        name="Felix"
+        phone="(212) 555-4567"
+        email="thecat@hotmail.com"
+      />
+      <Contact
+        img="./images/pumpkin.png"
+        name="Pumpkin"
+        phone="(0800) CAT KING"
+        email="pumpkin@scrimba.com"
+      />
     </div>
   );
 }
+
+export default App;
